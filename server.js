c8ad9dd1bb5e8e78478f5362b08be7f0cb5b3991
pittsblake <pittsblake1@gmail.com>
2017-10-13 14:53:08 -0400

    deployed to heroku

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ connection.on('connected', () => {
     console.log('Mongoose Connected Successfully');
 });
 
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
+
 // If the connection throws an error
 connection.on('error', (err) => {
     console.log('Mongoose default connection error: ' + err);
