b2f6b257753f5fe158cf37ea8735724678a4fd97
pittsblake <pittsblake1@gmail.com>
2017-10-14 14:03:12 -0400

    created homepage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
+// Importing React
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//Renaming BrowserRtouter to Router
+//Switch guarantees that we won't accidentally show multiple Routes at once
+import { BrowserRouter as Router , Switch, Route } from 'react-router-dom'
+
+//Importing components
+import HomePage from './components/home/HomePage'
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        {/* Router only allows one child component, so we wrap everything in a div. */}
+        <div>
+          {/* Anything outside of Switch is global and available in every view */}
+          {/* This is where I can add a NavBar or footer, so that it will be shown on every page */}
+          <Switch>
+            
+            <Route exact path='/' component={HomePage} />
+        
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Atlanta Rides</h1> 
+                <a href="">Users</a>  
+            </div>
+        );
+    }
+}
+
+export default HomePage;
