a2c958369c9b28003ec6eb616f393a826591dd98
pittsblake <pittsblake1@gmail.com>
2017-10-18 12:16:22 -0400

    styled home page and created logo to be a link to users

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 
 //Renaming BrowserRtouter to Router
 //Switch guarantees that we won't accidentally show multiple Routes at once
-import { BrowserRouter as Router , Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+//Importing Material UI
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+
 
 //Importing components
 import HomePage from './components/home/HomePage'
@@ -15,20 +21,22 @@ import UserProfile from './components/user/UserProfile'
 class App extends Component {
   render() {
     return (
-      <Router>
-        {/* Router only allows one child component, so we wrap everything in a div. */}
-        <div>
-          {/* Anything outside of Switch is global and available in every view */}
-          {/* This is where I can add a NavBar or footer, so that it will be shown on every page */}
-          <Switch>
+      <MuiThemeProvider>
+        <Router>
+          {/* Router only allows one child component, so we wrap everything in a div. */}
+          <div>
+            {/* Anything outside of Switch is global and available in every view */}
+            {/* This is where I can add a NavBar or footer, so that it will be shown on every page */}
+            <Switch>
 
-            <Route exact path='/' component={HomePage} />
-            <Route exact path='/users' component={LoginPage} />
-            <Route exact path='/users/:userId' component={UserProfile}/>
-        
-          </Switch>
-        </div>
-      </Router>
+              <Route exact path='/' component={HomePage} />
+              <Route exact path='/users' component={LoginPage} />
+              <Route exact path='/users/:userId' component={UserProfile} />
+
+            </Switch>
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
--- client/src/components/home/HomePage.jsx
+++ client/src/components/home/HomePage.jsx
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+// const MainContainer = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: flex-start;
+//     width: 100%;
+//     height: 100%;
+//     align-self: flex-start;
+    
+// `;
+
+const Title = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-self: flex-start;
+    z-index: 0;
+    height: 50vh
+`;
+
+const Logo = styled.h1 `
+    margin: 0px;
+    
+    
+    font-size: 10vw
+`;
+
+const A = styled.a `
+    text-decoration: none;
+    color: white;
+`;
+
+
 
 class HomePage extends Component {
     render() {
         return (
-            <div>
-                <h1>Atlanta Rides</h1> 
-                <Link to="/users">Users</Link>  
+            <div className="img">
+                    <Title>
+                        <Logo><A href="/users">Atlanta Rides</A></Logo>
+                        
+                    </Title>
             </div>
         );
     }
